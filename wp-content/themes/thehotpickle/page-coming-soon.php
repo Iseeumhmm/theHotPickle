33cb84fc8a1e8a79d8ca1d012bafab4546c0b5cc
Iseeumhmm <rickheffren@Ricks-MacBook-Pro.local>
2019-08-27 11:27:35 -0400

    coming soon

---
+++ wp-content/themes/thehotpickle/page-coming-soon.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package The_Hot_Pickle
+ */
+
+?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<div style="height: 100vh; width: 100vw;">
+				<h1 style="position: absolute; left: 50%; right: 50%; transform: translate(-50%, -50%);">Coming Soon!</h1>
+			</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php
+get_footer();
